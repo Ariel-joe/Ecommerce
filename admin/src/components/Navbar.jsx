f79487277ffb5ea284cfeb035d143aa10a4b4f1a
Ariel-Joe <arieljoe00@gmail.com>
2025-01-04 16:20:14 +0300

    add product done

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { assets } from "../assets/assets";
 const Navbar = ({setToken}) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
-      <img className="w-[80px] sm:w-[6%]" src={assets.primaryLogo} alt="" />
-      <button onClick={()=>setToken('')} className="bg-black text-white py-2 px-4 sm:px-8 sm:py-2 text-sm sm:text-base">Log Out</button>
+      <img className="w-[80px] sm:w-[4%]" src={assets.primaryLogo} alt="" />
+      <button onClick={()=>setToken('')} className="bg-black text-white py-2 px-4 sm:px-8 sm:py-2 text-sm sm:text-xs">Log Out</button>
     </div>
   );
 };
