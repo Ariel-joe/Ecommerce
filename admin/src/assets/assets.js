0c97256084c4c2b6dad764c0f5dc96a9d6da16f6
Ariel-Joe <arieljoe00@gmail.com>
2025-01-04 12:41:28 +0300

    add product page design

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,9 +1,11 @@
 import primaryLogo from "./egg_seller-removebg-logo.png"
 import secondaryLogo from "./egg-seller-bg-logo.png"
 import heroImage from "./hero-section.jpg"
+import uploadImage from "./icons8-upload-50.png"
 
 export const assets = {
     primaryLogo,
     secondaryLogo,
     heroImage,
+    uploadImage,
 }
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { assets } from "../assets/assets";
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between">
       <img className="w-[80px] sm:w-[6%]" src={assets.primaryLogo} alt="" />
-      <button className="bg-black text-white py-2 px-4 sm:px-8 sm:py-2 text-sm sm:text-base">Log Out</button>
+      <button onClick={()=>setToken('')} className="bg-black text-white py-2 px-4 sm:px-8 sm:py-2 text-sm sm:text-base">Log Out</button>
     </div>
   );
 };
