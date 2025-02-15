8139903306e49739565add6a1fbec6f93af19906
Ariel-Joe <arieljoe00@gmail.com>
2025-02-15 22:11:58 +0300

    zustand global state management

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import { Routes, Route } from "react-router-dom";
-import Add from "./pages/Add";
-import List from "./pages/List";
-import Orders from "./pages/Orders";
-import Login from "./components/Login";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+export const backendUrl = import.meta.env.VITE_BACKENDURL;
 
-export const backendUrl = import.meta.env.VITE_BACKENDURL
-
-export const currency = 'Ksh. '
+export const currency = "Ksh. ";
 
 const App = () => {
-  const [token, setToken] = useState(localStorage.getItem('token')? localStorage.getItem('token'): '');
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : ""
+  );
 
-  useEffect(()=> {
-    localStorage.setItem("token", token)
-  }, [token])
+  useEffect(() => {
+    localStorage.setItem("token", token);
+  }, [token]);
 
   return (
     <div className="bg-gray-50 min-h-screen">
@@ -34,15 +31,7 @@ const App = () => {
           <div className="flex w-full">
             <Sidebar />
             <div className="w-[70%] mx-auto ml-20px sm:ml-3vw my-4 text-gray-800 text-base">
-              <Routes>
-                <Route path="/dashboard" element={<Add token={token} />} />
-                <Route path="/add" element={<Add token={token} />} />
-                <Route path="/list" element={<List token={token} />} />
-                <Route path="/orders" element={<Orders token={token} />} />
-                <Route path="/payments" element={<Orders token={token} />} />
-                <Route path="/notifications" element={<Orders token={token} />} />
-                <Route path="/customers" element={<Orders token={token} />} />
-              </Routes>
+              <Outlet />
             </div>
           </div>
         </>
--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Orders = () => {
   return (
+    <>
     <div>
-      
+      this are the orders
     </div>
+      
+    </>
   )
 }
 
