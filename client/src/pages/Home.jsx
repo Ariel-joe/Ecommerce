6ab7bbc96783fef250280eebc542093628b27b7e
Ariel-Joe <arieljoe00@gmail.com>
2025-02-17 00:56:38 +0300

    hero-section implementation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { HeroSection } from "../components/HeroSection";
 
 const Home = () => {
   return (
     <>
-      <div>this is the home page</div>
+      <HeroSection />
     </>
   );
 };
