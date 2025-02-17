2a405f7a813057abfc4c40fde727a373ea610667
Ariel-Joe <arieljoe00@gmail.com>
2025-02-17 16:42:42 +0300

    policy section

--- client/src/components/Ourpolicy.jsx
+++ client/src/components/Ourpolicy.jsx
@@ -10,6 +10,18 @@ const Ourpolicy = () => {
             <p className="font-semibold">Easy Exchange Policy</p>
             <p className="text-gray-400">we offer hassle free exchange policy</p>
         </div>
+
+        <div>
+            <img src={assets.quality_icon} className="w-12 m-auto mb-5" alt="" />
+            <p className="font-semibold">7 days return policy</p>
+            <p className="text-gray-400">we offer provide 7 day free return policy</p>
+        </div>
+
+        <div>
+            <img src={assets.support_img} className="w-12 m-auto mb-5" alt="" />
+            <p className="font-semibold">Best customer support</p>
+            <p className="text-gray-400">we provide 24/7 customer support</p>
+        </div>
       </div>
     </>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HeroSection } from "../components/HeroSection";
 import { Latestcollection } from "../components/Latestcollection";
 import { Bestseller } from "../components/Bestseller";
+import { Ourpolicy } from "../components/Ourpolicy";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <Latestcollection />
       <Bestseller />
+      <Ourpolicy />
     </>
   );
 };
