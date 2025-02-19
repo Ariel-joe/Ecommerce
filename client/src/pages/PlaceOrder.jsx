c9ab3c8734667317fedaad9b2794069dd253c1e4
Ariel-Joe <arieljoe00@gmail.com>
2025-02-19 23:15:47 +0300

    stripe and razorpay integration

--- client/src/pages/PlaceOrder.jsx
+++ client/src/pages/PlaceOrder.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Title } from "../components/Title";
+import { CartTotal } from "../components/CartTotal";
+import { assets } from "../assets/assets";
 
 const PlaceOrder = () => {
   return (
@@ -24,12 +26,12 @@ const PlaceOrder = () => {
             </div>
 
             <input
-              className="border border-gray-300 rounded py-1.5 px-3.5 w-full text-sm"
+              className="border border-gray-300 mb-4 rounded py-1.5 px-3.5 w-full text-sm"
               type="email"
               placeholder="email"
             />
             <input
-              className="border border-gray-300 rounded py-1.5 px-3.5 w-full text-sm"
+              className="border border-gray-300 mb-4 rounded py-1.5 px-3.5 w-full text-sm"
               type="text"
               placeholder="street"
             />
@@ -56,13 +58,51 @@ const PlaceOrder = () => {
               <input
                 className="border border-gray-300 rounded py-1.5 px-3.5 w-full text-sm"
                 type="text"
-                placeholder="Last name"
+                placeholder="Country"
               />
             </div>
+
+            <input
+              className="border border-gray-300 rounded py-1.5 px-3.5 w-full text-sm"
+              type="number"
+              placeholder="phone"
+            />
           </div>
         </div>
 
         {/* --------right side--------- */}
+
+        <div className="mt-8">
+          <div className="mt-8 min-w-80">
+            <CartTotal />
+          </div>
+
+          <div className="mt-12">
+            <Title text1={"PAYMENT"} text2={"METHOD"} />
+
+            <div className="flex gap-3 flex-col lg:flex-row">
+              {/* stripe payments */}
+              <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+
+                <img className="h-5 mx-4" src={assets.stripe_logo} alt="" />
+              </div>
+
+              {/* razor payments */}
+              <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+
+                <img className="h-5 mx-4" src={assets.razorpay_logo} alt="" />
+              </div>
+
+              {/* cash on delivery */}
+              <div className="flex items-center gap-3 border py-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
