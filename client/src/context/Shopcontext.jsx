e5b035204692bca0080cd04676583fd646534874
Ariel-Joe <arieljoe00@gmail.com>
2025-02-19 13:57:02 +0300

    cart implementation

--- client/src/context/Shopcontext.jsx
+++ client/src/context/Shopcontext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -6,8 +6,31 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "Ksh ";
   const delivery_fee = 1000;
-  const [search, setSearch] = useState('')
-  const [showSearch, setShowSearch] = useState(false)
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  };
+
+  useEffect(() => {
+    console.log(cartItems);
+    
+  }, [cartItems])
 
   const value = {
     products,
@@ -17,6 +40,8 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
+    addToCart,
+    cartItems,
   };
 
   return (
